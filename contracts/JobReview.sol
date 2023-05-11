0fd4f76814c9f391a9b58ff29d4b601931a2c312
Oprea Laurentiu <oprealaur@yahoo.com>
2023-05-11 13:24:34 +0300

    code cleanup

--- contracts/JobReview.sol
+++ contracts/JobReview.sol
@@ -1,20 +1,31 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
+
+import "./JOBcoin.sol";
 
 contract JobReview {
+    uint counterReviews;
+    uint counterListings;
+
+    JOBcoin private _jobCoin;
+
     struct Review {
         uint id;
+        uint jobId;
         address reviewer;
         string companyName;
         string jobTitle;
         bytes32 reviewHash;
         string reviewText;
+        uint salary;
         uint8 rating;
     }
 
     struct JobListing {
+        uint id;
         string companyName;
         string jobTitle;
-        uint256 salary;
+        string description;
+        address poster;
     }
 
     Review[] public reviews;
@@ -27,22 +38,42 @@ contract JobReview {
     event JobListingCreated(
         string companyName,
         string jobTitle,
-        uint256 salary
+        string description,
+        address poster
     );
     event ReviewSubmitted(
+        uint reviewId,
         address indexed reviewer,
         string companyName,
         string jobTitle,
         bytes32 reviewHash,
         string reviewText,
+        uint salary,
         uint8 rating
     );
-    event ReviewDeleted(
+
+    event ReviewUpdated(
+        uint reviewId,
         address indexed reviewer,
         string companyName,
         string jobTitle,
-        bytes32 reviewHash
+        bytes32 reviewHash,
+        string reviewText,
+        uint salary,
+        uint8 rating
     );
+
+    event ReviewDeleted(
+        uint reviewId,
+        address indexed reviewer,
+        string companyName,
+        string jobTitle,
+        bytes32 reviewHash,
+        uint salary,
+        uint8 rating
+    );
+
+    event LogMessage(string message);
 
     modifier onlyOwner() {
         require(
@@ -54,33 +85,114 @@ contract JobReview {
 
     modifier onlyAuthorizedLister() {
         require(
-            authorizedListers[msg.sender],
+            authorizedListers[msg.sender] || msg.sender == owner,
             "Only authorized listers can call this function"
         );
         _;
     }
 
-    constructor() {
+    modifier onlyJobListingPoster(uint _id) {
+        require(_id < jobListings.length, "JobListing does not exist");
+        require(
+            jobListings[_id].poster == msg.sender,
+            "Only job listing poster can call this function"
+        );
+        _;
+    }
+
+    constructor(address jobCoinAddress) {
+        _jobCoin = JOBcoin(jobCoinAddress);
         owner = msg.sender;
+        resetCounters();
+    }
+
+    function resetCounters() public {
+        counterReviews = 0;
+        counterListings = 0;
     }
 
     function createJobListing(
         string memory _companyName,
         string memory _jobTitle,
-        uint256 _salary
+        string memory _desc
     ) public onlyAuthorizedLister {
-        JobListing memory newJob = JobListing(_companyName, _jobTitle, _salary);
+        require(
+            _jobCoin.balanceOf(msg.sender) >= 0.5 ether,
+            "Insufficient JOBcoins"
+        );
+
+        // Deduct 0.5 JOBcoins from the sender's balance
+        _jobCoin.transferFrom(msg.sender, owner, 0.5 ether);
+
+        JobListing memory newJob = JobListing(
+            counterListings++,
+            _companyName,
+            _jobTitle,
+            _desc,
+            msg.sender
+        );
         jobListings.push(newJob);
-        emit JobListingCreated(_companyName, _jobTitle, _salary);
+        emit JobListingCreated(_companyName, _jobTitle, _desc, msg.sender);
     }
 
-    uint counter = 0;
+    function getJobListingsCount() public view returns (uint256) {
+        return jobListings.length;
+    }
+
+    function getJobListing(
+        uint256 _index
+    ) public view returns (string memory, string memory, string memory) {
+        require(_index < jobListings.length, "Index out of range");
+        JobListing memory job = jobListings[_index];
+        return (job.companyName, job.jobTitle, job.description);
+    }
+
+    function updateJobListing(
+        uint _id,
+        string memory _companyName,
+        string memory _jobTitle,
+        string memory _desc
+    ) public {
+        require(_id < jobListings.length, "JobListing does not exist");
+        require(
+            bytes(_companyName).length > 0,
+            "Company name must not be empty"
+        );
+        require(bytes(_jobTitle).length > 0, "Job title must not be empty");
+
+        jobListings[_id].companyName = _companyName;
+        jobListings[_id].jobTitle = _jobTitle;
+        jobListings[_id].description = _desc;
+    }
+
+    function deleteJobListing(uint _id) public onlyJobListingPoster(_id) {
+        require(_id < jobListings.length);
+
+        require(
+            _jobCoin.balanceOf(msg.sender) >= 0.5 ether,
+            "Insufficient JOBcoins"
+        );
+
+        // Deduct 0.5 JOBcoins from the sender's balance
+        _jobCoin.transferFrom(msg.sender, owner, 0.5 ether);
+
+        for (uint i = reviews.length; i > 0; i--) {
+            if (reviews[i - 1].jobId == _id) {
+                deleteReview(i - 1);
+            }
+        }
+
+        jobListings[_id] = jobListings[jobListings.length - 1];
+
+        jobListings.pop();
+    }
 
     function submitReview(
         string memory _companyName,
         string memory _jobTitle,
         bytes32 _reviewHash,
         string memory _reviewText,
+        uint _salary,
         uint8 _rating
     ) public {
         require(
@@ -91,53 +203,84 @@ contract JobReview {
 
         require(_reviewHash != bytes32(0), "Hash must not be empty");
         require(_rating >= 1 && _rating <= 5, "Invalid rating");
-
+        uint myid = counterReviews++;
         Review memory newReview = Review(
-            counter++,
+            myid,
+            999999999,
             msg.sender,
             _companyName,
             _jobTitle,
             _reviewHash,
             _reviewText,
+            _salary,
             _rating
         );
         reviews.push(newReview);
         emit ReviewSubmitted(
+            myid,
             msg.sender,
             _companyName,
             _jobTitle,
             _reviewHash,
             _reviewText,
+            _salary,
             _rating
         );
     }
 
-    function getJobListingsCount() public view returns (uint256) {
-        return jobListings.length;
-    }
+    function createReview(
+        uint _jobId,
+        bytes32 _reviewHash,
+        string memory _reviewText,
+        uint _salary,
+        uint8 _rating
+    ) public {
+        require(_jobId < jobListings.length, "JobListing does not exist");
 
-    function getJobListing(
-        uint256 _index
-    ) public view returns (string memory, string memory, uint256) {
-        require(_index < jobListings.length, "Index out of range");
-        JobListing memory job = jobListings[_index];
-        return (job.companyName, job.jobTitle, job.salary);
-    }
+        JobListing memory job = jobListings[_jobId];
+        uint myid = counterReviews++;
+        Review memory newReview = Review({
+            id: myid,
+            jobId: _jobId,
+            reviewer: msg.sender,
+            companyName: job.companyName,
+            jobTitle: job.jobTitle,
+            reviewHash: _reviewHash,
+            reviewText: _reviewText,
+            rating: _rating,
+            salary: _salary
+        });
 
-    function getReviewsCount() public view returns (uint256) {
-        return reviews.length;
+        reviews.push(newReview);
+
+        emit ReviewSubmitted(
+            myid,
+            msg.sender,
+            jobListings[_jobId].companyName,
+            jobListings[_jobId].jobTitle,
+            _reviewHash,
+            _reviewText,
+            _salary,
+            _rating
+        );
     }
 
     function getReview(
         uint256 _index
-    ) public view returns (address, string memory, string memory, bytes32) {
+    )
+        public
+        view
+        returns (address, string memory, string memory, bytes32, uint, uint8)
+    {
         require(_index < reviews.length, "Index out of range");
         Review memory review = reviews[_index];
         return (
             review.reviewer,
             review.companyName,
             review.jobTitle,
-            review.reviewHash
+            review.reviewHash,
+            review.salary,
+            review.rating
         );
     }
 
@@ -152,29 +295,89 @@ contract JobReview {
         revert("Review not found");
     }
 
+    function getReviewsCount() public view returns (uint256) {
+        return reviews.length;
+    }
+
+    function setReviewText(uint256 reviewId, string memory reviewText) public {
+        require(reviewId < reviews.length, "Review ID does not exist");
+        Review storage review = reviews[reviewId];
+        review.id = reviewId;
+        review.reviewText = reviewText;
+    }
+
+    function updateReview(
+        uint _reviewId,
+        bytes32 _reviewHash,
+        string memory _reviewText,
+        uint _salary,
+        uint8 _rating
+    ) public {
+        require(_reviewId < reviews.length, "Invalid review ID");
+
+        Review storage reviewToUpdate = reviews[_reviewId];
+
+        require(
+            msg.sender == reviewToUpdate.reviewer,
+            "Only reviewer can update review"
+        );
+        require(_reviewHash != bytes32(0), "Hash must not be empty");
+        require(_rating >= 1 && _rating <= 5, "Invalid rating");
+
+        reviewToUpdate.reviewHash = _reviewHash;
+        reviewToUpdate.reviewText = _reviewText;
+        reviewToUpdate.salary = _salary;
+        reviewToUpdate.rating = _rating;
+
+        emit ReviewUpdated(
+            reviewToUpdate.id,
+            reviewToUpdate.reviewer,
+            reviewToUpdate.companyName,
+            reviewToUpdate.jobTitle,
+            reviewToUpdate.reviewHash,
+            reviewToUpdate.reviewText,
+            reviewToUpdate.salary,
+            reviewToUpdate.rating
+        );
+    }
+
     function deleteReview(uint256 _index) public {
         require(_index < reviews.length, "Index out of range");
         Review memory reviewToDelete = reviews[_index];
-        require(
-            reviewToDelete.reviewer == msg.sender,
-            "Only review owner can delete the review"
-        );
 
-        // Move the last element to the position of the element to be deleted
         Review memory lastReview = reviews[reviews.length - 1];
         reviews[_index] = lastReview;
 
-        // Reduce the length of the array by one
         reviews.pop();
+        counterReviews--;
 
-        // Emit the ReviewDeleted event
         bytes32 reviewHash = reviewToDelete.reviewHash;
         emit ReviewDeleted(
+            reviewToDelete.id,
             msg.sender,
             reviewToDelete.companyName,
             reviewToDelete.jobTitle,
-            reviewHash
+            reviewHash,
+            reviewToDelete.salary,
+            reviewToDelete.rating
         );
+    }
+
+    function deleteReviewsByReviewer(address _reviewer) public {
+        for (uint256 i = 0; i < reviews.length; i++) {
+            if (reviews[i].reviewer == _reviewer) {
+                delete reviews[i];
+                emit ReviewDeleted(
+                    reviews[i].id,
+                    _reviewer,
+                    reviews[i].companyName,
+                    reviews[i].jobTitle,
+                    reviews[i].reviewHash,
+                    reviews[i].salary,
+                    reviews[i].rating
+                );
+            }
+        }
     }
 
     function addAuthorizedLister(address _lister) public onlyOwner {
@@ -185,40 +388,118 @@ contract JobReview {
         delete authorizedListers[_lister];
     }
 
-    function isAuthorizedLister(address _lister) public view returns (bool) {
-        return authorizedListers[_lister];
-    }
-
     function transferOwnership(address _newOwner) public onlyOwner {
         owner = _newOwner;
     }
 
-    function deleteReviewsByReviewer(address _reviewer) public {
-        for (uint256 i = 0; i < reviews.length; i++) {
-            if (reviews[i].reviewer == _reviewer) {
-                delete reviews[i];
-                emit ReviewDeleted(
-                    _reviewer,
-                    reviews[i].companyName,
-                    reviews[i].jobTitle,
-                    reviews[i].reviewHash
-                );
-            }
-        }
+    function isAuthorizedLister(address _lister) public view returns (bool) {
+        return authorizedListers[_lister];
     }
 
-    function setReviewText(uint256 reviewId, string memory reviewText) public {
-        require(reviewId < reviews.length, "Review ID does not exist");
-        Review storage review = reviews[reviewId];
-        review.id = reviewId;
-        review.reviewText = reviewText;
-    }
-
-    function getBytes32Length(bytes32 data) public pure returns (uint256) {
+    function getBytes32Length(bytes32 data) internal pure returns (uint256) {
         bytes memory bytesData = new bytes(32);
         assembly {
             mstore(add(bytesData, 32), data)
         }
         return bytesData.length;
+    }
+
+    function getAllReviewsOfJobListing(
+        uint _jobId
+    ) public view returns (Review[] memory) {
+        Review[] memory jobReviews;
+        uint jobReviewsCounter = 0;
+        for (uint i = 0; i < reviews.length; i++) {
+            if (reviews[i].jobId == _jobId) {
+                jobReviewsCounter++;
+            }
+        }
+        jobReviews = new Review[](jobReviewsCounter);
+        jobReviewsCounter = 0;
+        for (uint i = 0; i < reviews.length; i++) {
+            if (reviews[i].jobId == _jobId) {
+                jobReviews[jobReviewsCounter] = reviews[i];
+                jobReviewsCounter++;
+            }
+        }
+        return jobReviews;
+    }
+
+    function _getSalaryStats(
+        string memory _jobTitle,
+        string memory _companyName
+    ) internal view returns (uint, uint, uint) {
+        uint totalSalary = 0;
+        uint minSalary = 99999999999;
+        uint maxSalary = 0;
+        uint numReviews = 0;
+        for (uint i = 0; i < reviews.length; i++) {
+            if (
+                (keccak256(bytes(reviews[i].jobTitle)) ==
+                    keccak256(bytes(_jobTitle)) &&
+                    keccak256(bytes(reviews[i].companyName)) ==
+                    keccak256(bytes(_companyName))) ||
+                (bytes(_jobTitle).length == 0 &&
+                    keccak256(bytes(reviews[i].companyName)) ==
+                    keccak256(bytes(_companyName))) ||
+                (
+                    (keccak256(bytes(reviews[i].jobTitle)) ==
+                        keccak256(bytes(_jobTitle)) &&
+                        bytes(_companyName).length == 0)
+                )
+            ) {
+                totalSalary += reviews[i].salary;
+                minSalary = (reviews[i].salary < minSalary)
+                    ? reviews[i].salary
+                    : minSalary;
+                maxSalary = (reviews[i].salary > maxSalary)
+                    ? reviews[i].salary
+                    : maxSalary;
+                numReviews++;
+            }
+        }
+        require(numReviews > 0, "No reviews found");
+        uint avgSalary = totalSalary / numReviews;
+        return (avgSalary, minSalary, maxSalary);
+    }
+
+    function getAllReviewsByReviewer(
+        address _reviewer
+    ) public view returns (Review[] memory) {
+        Review[] memory reviewerReviews;
+        uint reviewerReviewsCounter = 0;
+        for (uint i = 0; i < reviews.length; i++) {
+            if (reviews[i].reviewer == _reviewer) {
+                reviewerReviewsCounter++;
+            }
+        }
+        reviewerReviews = new Review[](reviewerReviewsCounter);
+        reviewerReviewsCounter = 0;
+        for (uint i = 0; i < reviews.length; i++) {
+            if (reviews[i].reviewer == _reviewer) {
+                reviewerReviews[reviewerReviewsCounter] = reviews[i];
+                reviewerReviewsCounter++;
+            }
+        }
+        return reviewerReviews;
+    }
+
+    function getCompanySalaryStats(
+        string memory _companyName
+    ) public view returns (uint, uint, uint) {
+        return _getSalaryStats("", _companyName);
+    }
+
+    function getJobTitleSalaryStats(
+        string memory _jobTitle
+    ) public view returns (uint, uint, uint) {
+        return _getSalaryStats(_jobTitle, "");
+    }
+
+    function getJobTitleSalaryStatsAtCompany(
+        string memory _jobTitle,
+        string memory _companyName
+    ) public view returns (uint, uint, uint) {
+        return _getSalaryStats(_jobTitle, _companyName);
     }
 }
--- test/jobreview.test.js
+++ test/jobreview.test.js
@@ -1,52 +1,58 @@
 const JobReview = artifacts.require("JobReview");
+const JOBcoin = artifacts.require("JOBcoin");
 const truffleAssert = require("truffle-assertions");
 
-contract("JobReview", (accounts) => {
-    let reviewContract; // Declare a variable to hold the contract instance
+contract("Jobchain - Job Reviews", (accounts) => {
+    let reviewContract;
 
-    // Before each test, deploy a new instance of the contract
     beforeEach(async () => {
-        reviewContract = await JobReview.new();
+        jobcoin = await JOBcoin.new("JOBcoin", "JOB", 1000000);
+        const tokenInstance = await JOBcoin.deployed();
+
+        const initialBalance = web3.utils.toWei("10", "ether"); // Set initial balance to 10 JOBcoins
+        const testUser1 = accounts[1];
+        const testUser2 = accounts[2];
+
+        await tokenInstance.transfer(testUser1, initialBalance, { from: accounts[0] });
+        await tokenInstance.transfer(testUser2, initialBalance, { from: accounts[0] });
+
+
+        await tokenInstance.approve(testUser1, initialBalance, { from: accounts[0] });
+        await tokenInstance.approve(testUser2, initialBalance, { from: accounts[0] });
+
+        reviewContract = await JobReview.new(tokenInstance.address);
     });
 
     it("test retrieve review text by review hash", async () => {
-        // Create a new review
         const reviewHash = "0x1234abcd";
         const reviewText = "This is a review.";
-        await reviewContract.submitReview("Company XYZ", "Job Title", reviewHash, reviewText, 5, { from: accounts[0] });
+        await reviewContract.submitReview("Company XYZ", "Job Title", reviewHash, reviewText, 1000, 5, { from: accounts[0] });
 
-        // Retrieve review text by review hash
         const retrievedReviewText = await reviewContract.getReviewText(reviewHash);
         assert.equal(retrievedReviewText, reviewText, "Review text not retrieved correctly");
     });
 
 
-    // Test submitting a review with valid inputs
     it("should submit a review with valid inputs", async () => {
         const companyName = "Company A";
         const jobTitle = "Job Title A";
         const reviewHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
+        const salary = 1000;
         const rating = 4;
 
-        // Submit a review
-        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, rating);
+        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, salary, rating);
 
-        // Get the review count
         const reviewCount = await reviewContract.getReviewsCount();
 
-        // Verify that the review count has increased by 1
         assert.equal(reviewCount.toNumber(), 1, "Review count not incremented correctly");
 
-        // Get the review details
         const review = await reviewContract.reviews(0);
 
-        // Verify that the review details match the submitted inputs
         assert.equal(review.companyName, companyName, "Company name not matching");
         assert.equal(review.jobTitle, jobTitle, "Job title not matching");
         assert.equal(review.reviewHash, reviewHash, "Review hash not matching");
         assert.equal(review.rating.toNumber(), rating, "Rating not matching");
 
-        // Check the emitted event
         const events = await reviewContract.getPastEvents("ReviewSubmitted");
         assert.equal(events.length, 1, "ReviewSubmitted event not emitted");
         assert.equal(events[0].args.reviewer, accounts[0], "Reviewer address not matching");
@@ -55,88 +61,73 @@ contract("JobReview", (accounts) => {
         assert.equal(events[0].args.reviewHash, reviewHash, "Review hash not matching in event");
     });
 
-    // Test submitting a review with invalid inputs
     it("should not submit a review with invalid inputs", async () => {
-        // Test submitting with empty strings
         try {
-            await reviewContract.submitReview("", "Job Title B", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 3);
+            await reviewContract.submitReview("", "Job Title B", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 1000, 3);
             assert.fail("Expected to fail with 'Invalid inputs', but transaction succeeded");
         } catch (error) {
             assert.include(error.message, "Company name must not be empty", "Error message does not match");
         }
-    
+
         try {
-            await reviewContract.submitReview("Company C", "", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 5);
+            await reviewContract.submitReview("Company C", "", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 1000, 5);
             assert.fail("Expected to fail with 'Invalid inputs', but transaction succeeded");
         } catch (error) {
             assert.include(error.message, "Job title must not be empty", "Error message does not match");
         }
-    
+
         try {
-            await reviewContract.submitReview("Company D", "Job Title D", "0x0000000000000000000000000000000000000000000000000000000000000000", { from: accounts[1] }, 2);
+            await reviewContract.submitReview("Company D", "Job Title D", "0x0000000000000000000000000000000000000000000000000000000000000000", { from: accounts[1] }, 1000, 2);
             assert.fail("Expected to fail with 'Invalid inputs', but transaction succeeded");
         } catch (error) {
             assert.include(error.message, "Hash must not be empty", "Error message does not match");
         }
-    
-    
-        // Test submitting with invalid rating
+
         try {
-            await reviewContract.submitReview("Company F", "Job Title F", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 6);
+            await reviewContract.submitReview("Company F", "Job Title F", "0x1234567890123456789012345678901234567890123456789012345678901234", { from: accounts[1] }, 1000, 6);
             assert.fail("Expected to fail with 'Invalid inputs', but transaction succeeded");
         } catch (error) {
             assert.include(error.message, "Invalid rating", "Error message does not match");
         }
     });
-    
 
-    // Test retrieving review text from hash
+
     it("should retrieve review text from hash", async () => {
         const companyName = "Company G";
         const jobTitle = "Job Title G";
         const reviewHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
         const rating = 4;
         const reviewText = "This is a sample review text";
+        const salary = 1000;
 
-        // Submit a review
-        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, rating);
+        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, salary, rating);
 
-        // Set review text for the submitted review
         await reviewContract.setReviewText(0, reviewText, { from: accounts[0] });
 
-        // Get review text from hash
         const retrievedReviewText = await reviewContract.getReviewText(reviewHash);
 
-        // Verify that the retrieved review text matches the submitted review text
         assert.equal(retrievedReviewText, reviewText, "Review text not retrieved correctly");
     });
 
-    // Test deleting a review by reviewer
     it("should delete a review by reviewer", async () => {
         const companyName = "Company H";
         const jobTitle = "Job Title H";
         const reviewHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
         const rating = 4;
+        const salary = 1000;
 
-        // Submit a review
-        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, rating);
+        await reviewContract.submitReview(companyName, jobTitle, reviewHash, { from: accounts[0] }, salary, rating);
 
-        // Get the review count
         let reviewCount = await reviewContract.getReviewsCount();
 
-        // Verify that the review count has increased by 1
         assert.equal(reviewCount.toNumber(), 1, "Review count not incremented correctly");
 
-        // Delete the review by reviewer
         await reviewContract.deleteReview(0, { from: accounts[0] });
 
-        // Get the updated review count
         reviewCount = await reviewContract.getReviewsCount();
 
-        // Verify that the review count has decreased by 1
         assert.equal(reviewCount.toNumber(), 0, "Review count not decremented correctly");
 
-        // Try to retrieve the deleted review
         await truffleAssert.reverts(
             reviewContract.reviews(0),
             null, // null to ignore expected message
